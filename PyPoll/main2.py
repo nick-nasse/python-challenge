d86b2fae1fa408c4235a7e56d9b4cf273a762ad5
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-06-30 12:43:56 -0700

    updated PyPoll so the names AREN'T hardcoded and discover the candidate names as the file processes after feedback from Ryan Buss, TA

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -4,65 +4,41 @@ import csv
 #file path
 election_path = "Resources/election_data.csv"
 
-#Variables 
-
+#variables
+d = {}
 total_votes = 0 
-khan_votes = 0
-correy_votes = 0
-li_votes = 0
-otooley_votes = 0 
 
 with open(election_path, "r") as csvfile:
     election_csv = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvfile)
-    
+
     for row in election_csv:
         total_votes += 1
 
-        if row[2] == "Khan":
-            khan_votes += 1
-        
-        elif row[2] == "Correy":
-            correy_votes += 1 
-        
-        elif row[2] == "Li":
-            li_votes += 1
-        
-        elif row[2] == "O'Tooley":
-            otooley_votes += 1
-    
+        if row[2] in d:
+            d["%s" % row[2]] += 1
 
-khan_percentage = "{:.2f}".format(khan_votes / total_votes * 100)
+        else: 
+            d["%s" % row[2]] = 1
 
-correy_percentage = "{:.2f}".format(correy_votes / total_votes * 100)
-
-li_percentage = "{:.2f}".format(li_votes / total_votes * 100)
-
-otooley_percentage = "{:.2f}".format(otooley_votes / total_votes * 100)
 
 print(f"Election Results")
 print(f"------------------------------------")
 print(f"Total Votes: {total_votes}")
 print(f"------------------------------------")
-print(f"Khan: {khan_percentage}% ({khan_votes})")
-print(f"Correy: {correy_percentage}% ({correy_votes})")
-print(f"Li: {li_percentage}% ({li_votes})")
-print(f"O'Tooley: {otooley_percentage}% ({otooley_votes})")
+for key in d:
+    print(key,":", "{:.2f}".format(d[key]/total_votes*100), "%", "(", d[key], ")")
+print(f"------------------------------------")
+print("Winner:", max(d, key=d.get))
 print(f"------------------------------------")
 
-if khan_votes > correy_votes and khan_votes > li_votes and khan_votes > otooley_votes:
-    print("Winner: Khan")
-elif correy_votes > khan_votes and correy_votes > li_votes and correy_votes > otooley_votes:
-    print("Winner: Correy")
-elif li_votes > khan_votes and li_votes > correy_votes and li_votes > otooley_votes:
-    print("Winner: Li")
-elif otooley_votes > khan_votes and otooley_votes > li_votes and otooley_votes > correy_votes:
-    print("Winner: O'Tooley")
 
-print(f"------------------------------------")
 
-winner = "Khan"
+text_iteration = []
+
+for key in d: 
+    text_iteration.append(f"{key} : {round(d[key]/total_votes*100, 2)}% ({d[key]})")
 
 text_path = "Analysis/PyPoll_Results.txt"
 
@@ -75,17 +51,11 @@ text_file.write(f"Total Votes: {total_votes}")
 text_file.write("\n")
 text_file.write(f"------------------------------------")
 text_file.write("\n")
-text_file.write(f"Khan: {khan_percentage}% ({khan_votes})")
-text_file.write("\n")
-text_file.write(f"Correy: {correy_percentage}% ({correy_votes})")
-text_file.write("\n")
-text_file.write(f"Li: {li_percentage}% ({li_votes})")
-text_file.write("\n")
-text_file.write(f"O'Tooley: {otooley_percentage}% ({otooley_votes})")
-text_file.write("\n")
+for texty in text_iteration:
+    text_file.write(texty)
+    text_file.write("\n")
 text_file.write(f"------------------------------------")
 text_file.write("\n")
-text_file.write("Winner: " + winner)
+text_file.write(f"Winner: {max(d, key=d.get)}")
 text_file.write("\n")
 text_file.write(f"------------------------------------")
-text_file.close() 
